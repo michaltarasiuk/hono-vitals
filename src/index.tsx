9c126e20e61cc75818f0a29ccae29dd0cee8f56e
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-07 17:58:15 +0200

    Feat: add JSX rendering, static file serving with delay

---
+++ src/index.tsx
@@ -0,0 +1,121 @@
+/** @jsx jsx */
+/** @jsxImportSource hono/jsx */
+
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { jsxRenderer } from "hono/jsx-renderer";
+import * as z from "zod";
+import { delay } from "./utils/delay";
+
+const app = new Hono();
+
+app.get(
+  "/static/*",
+  zValidator(
+    "query",
+    z.object({
+      delay: z.coerce.number().optional(),
+    }),
+  ),
+  async (context, next) => {
+    const { delay: timeout } = context.req.valid("query");
+    if (timeout !== undefined) {
+      await delay(timeout);
+    }
+    next();
+  },
+  serveStatic({
+    root: "./src",
+  }),
+);
+
+app.get(
+  "/page/*",
+  jsxRenderer(({ children }) => {
+    return (
+      <html>
+        <body>{children}</body>
+      </html>
+    );
+  }),
+);
+
+app.get("/page/about", (context) => {
+  return context.render(<h1>About me!</h1>);
+});
+
+app.post(
+  "/analytics",
+  zValidator(
+    "json",
+    z.object({
+      metric: z.lazy(() => MetricSchema),
+    }),
+  ),
+  (context) => {
+    context.req.valid("json");
+    return context.json({
+      ok: true,
+    });
+  },
+);
+
+export default app;
+
+const MetricSchema = z.object({
+  /**
+   * The name of the metric (in acronym form).
+   */
+  name: z.enum(["CLS", "FCP", "INP", "LCP", "TTFB"]),
+  /**
+   * The current value of the metric.
+   */
+  value: z.number(),
+  /**
+   * The rating as to whether the metric value is within the "good",
+   * "needs improvement", or "poor" thresholds of the metric.
+   */
+  rating: z.enum(["good", "needs-improvement", "poor"]),
+  /**
+   * The delta between the current value and the last-reported value.
+   * On the first report, `delta` and `value` will always be the same.
+   */
+  delta: z.number(),
+  /**
+   * A unique ID representing this particular metric instance. This ID can
+   * be used by an analytics tool to dedupe multiple values sent for the same
+   * metric instance, or to group multiple deltas together and calculate a
+   * total. It can also be used to differentiate multiple different metric
+   * instances sent from the same page, which can happen if the page is
+   * restored from the back/forward cache (in that case new metrics object
+   * get created).
+   */
+  id: z.string(),
+  /**
+   * Any performance entries relevant to the metric value calculation.
+   * The array may also be empty if the metric value was not based on any
+   * entries (e.g. a CLS value of 0 given no layout shifts).
+   */
+  entries: z.array(z.any()),
+  /**
+   * The type of navigation.
+   *
+   * This will be the value returned by the Navigation Timing API (or
+   * `undefined` if the browser doesn't support that API), with the following
+   * exceptions:
+   * - 'back-forward-cache': for pages that are restored from the bfcache.
+   * - 'back_forward' is renamed to 'back-forward' for consistency.
+   * - 'prerender': for pages that were prerendered.
+   * - 'restore': for pages that were discarded by the browser and then
+   * restored by the user.
+   */
+  navigationType: z.enum([
+    "navigate",
+    "reload",
+    "back-forward",
+    "back-forward-cache",
+    "prerender",
+    "restore",
+  ]),
+});
