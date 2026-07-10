61ceee472419bfbbc9018b4bafeb9ecac07aab50
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-10 23:15:03 +0200

    Revert empty-string preprocess on queryNumberDefault

--- utils/metric/flags/coerce.ts
+++ utils/metric/flags/coerce.ts
@@ -3,7 +3,4 @@ import * as z from "zod";
 export const queryBoolean = z.coerce.boolean().default(false);
 
 export const queryNumberDefault = (defaultValue: number) =>
-  z.preprocess(
-    (value) => (value === "" ? undefined : value),
-    z.coerce.number().default(defaultValue),
-  );
+  z.coerce.number().default(defaultValue);
