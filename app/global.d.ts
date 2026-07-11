6098af35c8585032ebafaf79cb6c5560c5a8905c
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-11 18:24:00 +0200

    extract design tokens and simplify component styles

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,1 +1,6 @@
 import "@hono/react-renderer";
+
+declare module "*.css?inline" {
+  const css: string;
+  export default css;
+}
--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,13 +1,19 @@
 import { reactRenderer } from "@hono/react-renderer";
 import { Link, Script } from "honox/server";
 
+import devStyles from "../style.css?inline";
+
 export default reactRenderer(({ children }) => {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Link href="/app/style.css" rel="stylesheet" />
+        {import.meta.env.DEV ? (
+          <style dangerouslySetInnerHTML={{ __html: devStyles }} />
+        ) : (
+          <Link href="/app/style.css" rel="stylesheet" />
+        )}
         <Script src="/app/client.ts" async />
       </head>
       <body>{children}</body>
