96ae4a9e044e929d1693427be72ea79c3fa3c9a7
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-11 13:04:04 +0200

    Put img src after identity and state props

--- app/routes/metric/fcp.tsx
+++ app/routes/metric/fcp.tsx
@@ -15,8 +15,8 @@ export default createRoute(zValidator("query", FcpFlagsSchema), (c) => {
       <h1 {...elementTiming("main-heading")}>FCP Test</h1>
       <p>
         <img
-          src={`/static/square.png?delay=${flags.imgDelay}`}
           hidden={flags.imgHidden}
+          src={`/static/square.png?delay=${flags.imgDelay}`}
           {...elementTiming("main-image")}
         />
       </p>
