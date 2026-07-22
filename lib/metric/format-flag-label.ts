69c824dbbf3a40ec63db5abdbfe87ae30715b1bc
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-22 23:41:03 +0200

    Enable noUncheckedIndexedAccess for safer indexed access

--- lib/metric/format-flag-label.ts
+++ lib/metric/format-flag-label.ts
@@ -1,5 +1,5 @@
 export function formatFlagLabel(key: string) {
-  const [first, ...rest] = key
+  const [first = "", ...rest] = key
     .replace(/([a-z])([A-Z])/g, "$1 $2")
     .replace(/([A-Za-z])(\d)/g, "$1 $2")
     .toLowerCase();
