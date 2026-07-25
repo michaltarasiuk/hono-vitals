3644f55657662749b24921d64231a45cd9c71381
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-25 17:43:04 +0200

    inline metric summary parsing and drop queryRows helper

--- lib/analytics/duckdb/query.ts
+++ lib/analytics/duckdb/query.ts
@@ -1,10 +1,3 @@
 import * as z from "zod";
 
 export const dbNumber = z.coerce.number();
-
-export async function queryRows<Schema extends z.ZodType>(
-  schema: Schema,
-  result: PromiseLike<unknown>,
-) {
-  return z.array(schema).parse(await result);
-}
