ee5055b94af68a1f13c49e2f658f38788d64241c
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-28 11:17:11 +0200

    Drop inter font preload from renderer

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,4 +1,3 @@
-import inter from "@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url";
 import { reactRenderer } from "@hono/react-renderer";
 import { Link, Script } from "honox/server";
 
@@ -11,13 +10,6 @@ export default reactRenderer(({ children }) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Link href="/public/favicon.svg" rel="icon" type="image/svg+xml" />
-        <Link
-          href={inter}
-          rel="preload"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
         {import.meta.env.DEV ? (
           <style dangerouslySetInnerHTML={{ __html: devStyles }} />
         ) : (
