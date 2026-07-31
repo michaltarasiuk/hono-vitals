02f4cb85cc2d0ee1692564caed5e1106f9760d24
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-31 16:30:37 +0200

    destructure navigation entry from performance timeline

--- lib/metric/override-response-start.ts
+++ lib/metric/override-response-start.ts
@@ -1,7 +1,7 @@
 import { isDefined } from "@/lib/is-defined";
 
 export function overrideResponseStart(responseStart: number) {
-  const navEntry = performance.getEntriesByType("navigation")[0];
+  const [navEntry] = performance.getEntriesByType("navigation");
   if (isDefined(navEntry)) {
     Object.defineProperty(navEntry, "responseStart", {
       value: responseStart,
