0a6d16c5ca42ec5e54b7e06f3ab6a4f8885e06fb
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-02 20:28:50 +0200

    Migrate DuckDB schema on server boot

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,10 @@
 import { showRoutes } from "hono/dev";
 import { createApp } from "honox/server";
 
+import { migrate } from "@/lib/analytics/duckdb/schema";
+
+await migrate();
+
 const app = createApp();
 
 showRoutes(app);
