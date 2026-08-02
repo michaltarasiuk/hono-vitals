12a74ea3c5a263abace4ec32fea6376b55b757ef
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-02 21:10:54 +0200

    flush batched metrics on dispose instead of dropping them

--- lib/metric/batch-reporter.ts
+++ lib/metric/batch-reporter.ts
@@ -31,7 +31,7 @@ export function createBatchReporter() {
   function dispose() {
     document.removeEventListener("visibilitychange", onVisibilityChange);
     document.removeEventListener("pagehide", flush);
-    queue.clear();
+    flush();
   }
 
   return { enqueue, dispose };
