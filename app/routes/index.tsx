edaac6730a8dca4844a10741528419e944c19fdb
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-04 14:37:23 +0200

    Add Clear samples alert dialog to the dashboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ import { createRoute } from "honox/factory";
 
 import { MetricsSummary } from "@/app/components/dashboard/metrics-summary";
 import { Toolbar } from "@/app/components/layout/toolbar";
+import { ClearMetrics } from "@/app/islands/clear-metrics";
 import { getMetricsSummary } from "@/lib/analytics/metrics";
 
 export default createRoute(async (c) => {
@@ -11,6 +12,9 @@ export default createRoute(async (c) => {
     <>
       <Toolbar.Root>
         <Toolbar.Nav currentPath={c.req.path} />
+        <Toolbar.Actions>
+          <ClearMetrics />
+        </Toolbar.Actions>
       </Toolbar.Root>
       <main className="MetricMain">
         <MetricsSummary summaries={summaries} />
