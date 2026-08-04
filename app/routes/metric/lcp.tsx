f0c8ac5b8a1592e0976ee2ae89ed79ab7c976c2b
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-04 14:36:25 +0200

    Reorganize metric islands under app/islands

--- app/routes/metric/lcp.tsx
+++ app/routes/metric/lcp.tsx
@@ -2,7 +2,7 @@ import { FullHeight } from "@/app/components/metric/full-height";
 import { SquareImage } from "@/app/components/metric/square-image";
 import { Heading } from "@/app/components/ui/heading/heading";
 import { Text } from "@/app/components/ui/text/text";
-import { LcpObserver } from "@/app/islands/metric/lcp-observer";
+import { LcpObserver } from "@/app/islands/observers/lcp-observer";
 import { defineMetric } from "@/lib/metric/define-metric";
 import {
   LCP_FLAGS_DEFAULTS,
