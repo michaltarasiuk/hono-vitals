bd53cda3cdbbd0edc4291beaaf2c58a004162f5a
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-24 15:07:24 +0200

    Simplify InstanceFlag by stripping the 2 suffix from dual keys

--- lib/metric/observer-options.ts
+++ lib/metric/observer-options.ts
@@ -7,11 +7,8 @@ export type ObserverInstance = 1 | 2;
 type ObserverFlags = Pick<BaseFlags, 'reportAllChanges' | 'reportAllChanges2'> &
   Partial<GenerateTargetFlags & InpReportFlags>;
 
-type InstanceFlag = {
-  [K in keyof ObserverFlags]: `${K & string}2` extends keyof ObserverFlags
-    ? K
-    : never;
-}[keyof ObserverFlags];
+type Without2<K> = K extends `${infer Flag}2` ? Flag : never;
+type InstanceFlag = Without2<keyof ObserverFlags>;
 
 export function observerOptions(
   flags: ObserverFlags,
