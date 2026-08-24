a47f8a77e5ffa63851b70de28bbcf50bedd31e98
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-24 12:14:56 +0200

    Reuse flagsToSearchParams when navigating with flag query updates

--- lib/metric/flags/navigate-with-flags.ts
+++ lib/metric/flags/navigate-with-flags.ts
@@ -1,17 +1,9 @@
-import {isNonDefaultFlag} from './search-params';
+import {flagsToSearchParams} from './search-params';
 
 import type {Flags} from './schema';
 
 export function navigateWithFlags(flags: Flags, defaults: Flags) {
   const url = new URL(location.href);
-
-  for (const [key, value] of Object.entries(flags)) {
-    if (isNonDefaultFlag(value, defaults[key])) {
-      url.searchParams.set(key, String(value));
-    } else {
-      url.searchParams.delete(key);
-    }
-  }
-
+  url.search = flagsToSearchParams(flags, defaults).toString();
   location.assign(url);
 }
