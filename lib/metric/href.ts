33549fc83f0ffa815854f8426d8fedb13df510d1
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-24 18:47:08 +0200

    Move important up

--- lib/metric/href.ts
+++ lib/metric/href.ts
@@ -4,10 +4,6 @@ import {flagsToSearchParams} from '@/lib/metric/flags/search-params';
 import type {Flags} from '@/lib/metric/flags/schema';
 import type {MetricName} from '@/lib/metric/schema';
 
-export function metricSlug(metricName: MetricName) {
-  return metricName.toLowerCase() as Lowercase<MetricName>;
-}
-
 export function metricHref(
   metricName: MetricName,
   flags: Flags = {},
@@ -16,4 +12,8 @@ export function metricHref(
   const path = joinPath('metric', metricSlug(metricName));
   const query = flagsToSearchParams(flags, defaults);
   return query.size > 0 ? `${path}?${query}` : path;
+}
+
+export function metricSlug(metricName: MetricName) {
+  return metricName.toLowerCase() as Lowercase<MetricName>;
 }
